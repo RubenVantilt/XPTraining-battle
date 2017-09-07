79f0fc17a5a91a1b459552cb01acd0031a30caff
rubeva <rubeva@HI95895>
2017-09-07 15:25:59 +0200

    US5

--- Battle.Tests/ArmyTest.cs
+++ Battle.Tests/ArmyTest.cs
@@ -29,5 +29,39 @@ namespace Battle.Tests
 
             army.Frontman.Should().Be(frontManSoldier);
         }
+
+        [Fact]
+        public void GivenTwoArmiesWithOneSoldier_WhenFightingWar_StrongestFrontManWins()
+        {
+            var firstArmy = new Army();
+            var secondArmy = new Army();
+
+            var frontmanFirstArmy = new Soldier("Front man first army", Weapon.BAREFIST);
+            var frontmanSecondArmy = new Soldier("Front man second army", Weapon.SPEAR);
+
+            firstArmy.Enroll(frontmanFirstArmy);
+            secondArmy.Enroll(frontmanSecondArmy);
+
+            var winningArmy = firstArmy.Attack(secondArmy);
+
+            winningArmy.Should().Be(secondArmy);
+        }
+
+        [Fact]
+        public void GivenTwoArmiesWithOneSoldier_WhenFightingWar_LoserDiesAndIsRemovedFromArmy()
+        {
+            var firstArmy = new Army();
+            var secondArmy = new Army();
+
+            var frontmanFirstArmy = new Soldier("Front man first army", Weapon.BAREFIST);
+            var frontmanSecondArmy = new Soldier("Front man second army", Weapon.SPEAR);
+
+            firstArmy.Enroll(frontmanFirstArmy);
+            secondArmy.Enroll(frontmanSecondArmy);
+
+            firstArmy.Attack(secondArmy);
+
+            firstArmy.Soldiers.Should().BeEmpty();
+        }
     }
 }
--- Battle/Army.cs
+++ Battle/Army.cs
@@ -19,5 +19,31 @@ namespace Battle
         public List<Soldier> Soldiers { get; set; }
 
         public Soldier Frontman => Soldiers.First();
+
+        public Army Attack(Army secondArmy)
+        {
+            var winningFrontman = Frontman.Fight(secondArmy.Frontman);
+            if (! isOurFrontman(winningFrontman))
+            {
+                this.RemoveDeadSoldier(Frontman);
+                return secondArmy;
+            }
+            else
+            {
+                secondArmy.RemoveDeadSoldier(secondArmy.Frontman);
+                return this;
+            }
+
+        }
+
+        private bool isOurFrontman(Soldier soldier)
+        {
+            return Frontman == soldier;
+        }
+
+        private void RemoveDeadSoldier(Soldier deadSoldier)
+        {
+            Soldiers.Remove(deadSoldier);
+        }
     }
 }
